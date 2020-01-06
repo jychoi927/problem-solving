39a1753823e876f9272a0dacce8ada942df7270c
jychoi927 <jychoi927@gmail.com>
2020-01-06 18:53:05 +0900

    [boj] 10953 - A+B - 6

---
+++ boj/input-output/10953.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a,b,test_case;
+    cin >> test_case;
+
+    while(test_case--){
+        scanf("%d,%d",&a,&b);
+        cout << a + b << "\n";
+    }
+}
