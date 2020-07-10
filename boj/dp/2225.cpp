0cb5b359591c58f831284099511fa2409ad8fe1e
jychoi927 <jychoi927@gmail.com>
2020-07-10 23:44:43 +0900

    [boj] 2225 - 합분해

---
+++ boj/dp/2225.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int d[201][201];
+
+int main(){
+    int n, k;
+    cin >> n >> k;
+    
+    for(int i=0;i<=k;i++){
+	d[0][i] = 1;
+    }
+    for(int i=1;i<=n;i++){
+	for(int j=1;j<=k;j++){
+            d[i][j] = (d[i-1][j] + d[i][j-1]) % 1000000000;
+	}
+    }
+
+    cout << d[n][k] << endl;
+    
+    return 0;
+}
