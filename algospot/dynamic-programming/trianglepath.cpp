bfcdeaac4018c4ee47656a2514b47b4125a79981
jychoi927 <jychoi927@gmail.com>
2020-08-11 08:56:49 +0900

    [algospot] 삼각형 위의 최대 경로

--- algospot/dynamic-programming/trianglepath.cpp
+++ algospot/dynamic-programming/trianglepath.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <algorithm>
+#include <cstring>
 
 #define endl '\n'
 #define fastio cin.sync_with_stdio(false); cin.tie(nullptr)
@@ -12,28 +13,28 @@ int d[MAX][MAX];
 int n;
 
 int dp(int x, int y){
-	if (y == n)
-		return arr[y][x];
-	if (d[y][x] != -1)
-		return d[y][x];
-	return d[y][x] = arr[y][x] + max(dp(x, y+1), dp(x+1, y+1));
+    if (y == n)
+        return arr[y][x];
+    if (d[y][x] != -1)
+        return d[y][x];
+    return d[y][x] = arr[y][x] + max(dp(x, y+1), dp(x+1, y+1));
 }
 
 int main(){
     fastio;
 
-	int _;
-	cin >> _;
-	while (_--){
-		memset(d, -1, sizeof(d));
+    int _;
+    cin >> _;
+    while (_--){
+        memset(d, -1, sizeof(d));
 
-		cin >> n;
-		for (int i=1;i<=n;i++)
-			for (int j=1;j<=i;j++)
-				cin >> arr[i][j];
+        cin >> n;
+        for (int i=1;i<=n;i++)
+            for (int j=1;j<=i;j++)
+                cin >> arr[i][j];
 
-		cout << dp(1, 1) << endl;
-	}
+        cout << dp(1, 1) << endl;
+    }
 
-	return 0;
+    return 0;
 }
