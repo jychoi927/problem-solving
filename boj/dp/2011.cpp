4e41ed732c1f356fc5aec90e743d7072c7fe004a
jychoi927 <jychoi927@gmail.com>
2020-09-13 23:41:06 +0900

    [boj] 2011 - 암호코드

--- boj/dp/2011.cpp
+++ boj/dp/2011.cpp
@@ -7,26 +7,26 @@ const int MOD = 1000000;
 long long d[5001];
 
 int main(){
-    string str;
-    cin >> str;
-    
-    int len = str.size();
-    str = " " + str;
-    
+    string s;
+    cin >> s;
+
+    int len = s.size();
+    s = " " + s;
+
     d[0] = 1;
     for(int i=1;i<=len;i++){
-        int x = str[i] - '0';
-        if(0<x && x<10)
+        int x = s[i] - '0';
+        if(0 < x)
             d[i] = (d[i] + d[i-1]) % MOD;
-        
-	if(i==1 || str[i-1] == '0')
+
+        if(i == 1 || s[i-1] == '0')
             continue;
-        
-        x = (str[i-1] - '0') * 10 + (str[i] - '0');
-        if(10<=x && x<=26)
+
+        x = (s[i-1] - '0') * 10 + (s[i] - '0');
+        if(10 <= x && x <= 26)
             d[i] = (d[i] + d[i-2]) % MOD;
     }
-    
+
     cout << d[len] << endl;
 
     return 0;
